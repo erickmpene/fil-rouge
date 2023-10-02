cfb2a4959271a8e960dcdde239d918c1cea8171b
erickmpene <erickmpene@gmail.com>
2023-10-02 16:30:59 +0000

    update

--- ressources/terraform/modules/eip/main.tf
+++ ressources/terraform/modules/eip/main.tf
@@ -1,7 +1,7 @@
 resource "aws_eip" "ic_webapp_pgadmin_eip" {
   instance = var.ec2_instance_ic-webapp_pgadmin
   provisioner "local-exec" {
-    command = "echo ${aws_eip.ic_webapp_pgadmin_eip.public_ip} > ./app/ic_webapp_pgadmin_eip.txt"
+    command = "echo ${aws_eip.ic_webapp_pgadmin_eip.public_ip} > ./ip_address/ic_webapp_pgadmin_eip.txt"
   }
   tags = {
     Name = "${var.prefix_name}-eip-ic_webapp_pgadmin"
@@ -11,7 +11,7 @@ resource "aws_eip" "ic_webapp_pgadmin_eip" {
 resource "aws_eip" "odoo_postgres_eip" {
   instance = var.ec2_instance_odoo_postgres_ec2
   provisioner "local-exec" {
-    command = "echo ${aws_eip.odoo_postgres_eip.public_ip} > ./app/odoo_postgres_eip.txt"
+    command = "echo ${aws_eip.odoo_postgres_eip.public_ip} > ./ip_address/odoo_postgres_eip.txt"
   }
   tags = {
     Name = "${var.prefix_name}-eip-odoo_postgres_eip"
