edec97b017bf839b8599969112ba1b4c2ca609a7
erickmpene <erickmpene@gmail.com>
2023-10-03 11:25:46 +0000

    update

--- ressources/terraform/modules/eip/main.tf
+++ ressources/terraform/modules/eip/main.tf
@@ -1,7 +1,7 @@
 resource "aws_eip" "prod_eip" {
   instance = var.ec2_instance_prod
   provisioner "local-exec" {
-    command = "echo ${aws_eip.prod_eip.public_ip} > ./ip_address/prod_eip.txt"
+    command = "sed -i '/^ansible_host:/ s/ansible_host: .*/ansible_host: ${aws_eip.prod_eip.public_ip}/' ../ansible-ressources/host_vars/prod_srv.yml"
   }
   tags = {
     Name = "${var.prefix_name}-eip-prod"
@@ -11,7 +11,7 @@ resource "aws_eip" "prod_eip" {
 resource "aws_eip" "staging_eip" {
   instance = var.ec2_instance_staging
   provisioner "local-exec" {
-    command = "echo ${aws_eip.staging_eip.public_ip} > ./ip_address/staging_eip.txt"
+    command = "sed -i '/^ansible_host:/ s/ansible_host: .*/ansible_host: ${aws_eip.staging_eip.public_ip}/' ../ansible-ressources/host_vars/staging_srv.yml"
   }
   tags = {
     Name = "${var.prefix_name}-eip-staging"
